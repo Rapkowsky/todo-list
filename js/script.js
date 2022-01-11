1ad93ce24efdf32c4e5df3869fb412208c68fb6c
Rapkowsky <robertrapkowski19@gmail.com>
2022-01-11 22:14:45 +0100

    Adding new element to the list

--- js/script.js
+++ js/script.js
@@ -23,12 +23,35 @@ const addNewTask = () => {
 	if (toDoInput.value !== "") {
 		newToDoElement = document.createElement("li");
 		newToDoElement.textContent = toDoInput.value;
+		createToolsPanel();
+
 		ulList.append(newToDoElement);
+
 		toDoInput.value = "";
 		errorMssg.textContent = "";
 	} else {
 		errorMssg.textContent = "Wpisz treść zadania!";
 	}
+};
+
+const createToolsPanel = () => {
+	const toolsContainer = document.createElement("div");
+	toolsContainer.classList.add("tools");
+	newToDoElement.append(toolsContainer);
+
+	const acceptButton = document.createElement("button");
+	acceptButton.classList.add("complete");
+	acceptButton.innerHTML = '<i class="fas fa-check"></i>';
+
+	const editButton = document.createElement("button");
+	editButton.classList.add("edit");
+	editButton.textContent = "EDIT";
+
+	const deleteButton = document.createElement("button");
+	deleteButton.classList.add("delete");
+	deleteButton.innerHTML = '<i class="fas fa-times"></i>';
+
+	toolsContainer.append(acceptButton, editButton, deleteButton);
 };
 
 document.addEventListener("DOMContentLoaded", main);
